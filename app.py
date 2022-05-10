c8bcb5a8a3f337038716d23f9cdd37f8dddfb012
Yujin Seol <66419086+rkdfc93@users.noreply.github.com>
2022-05-10 19:30:34 +0900

    url 받아 처리

--- app.py
+++ app.py
@@ -25,6 +25,15 @@ def predict():
         results = model(img, size=640)
         data = results.pandas().xyxy[0].to_json(orient="records")
         return data
+    if request.files.get("url"):
+        image_url = request.files["url"]
+        image_bytes = image_url.read()
+        print(image_url)
+        img = Image.open(io.BytesIO(image_bytes))
+
+        results = model(img, size=640)
+        data = results.pandas().xyxy[0].to_json(orient="records")
+        return data
 
 
 if __name__ == "__main__":
@@ -32,4 +41,6 @@ if __name__ == "__main__":
     model = torch.hub.load(
          "ultralytics/yolov5", "custom", path="last.pt", force_reload=True)
     model.eval()
-    app.run(host="0.0.0.0", port=5000) 
+    app.run(host="0.0.0.0", port=5000)
+    res = request.post("http://127.0.0.1:5000", files = {"url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/media/admin/%EC%BB%A8%ED%95%981734/22.jpg"}).json()
+    print(res)
