164c990de8d937536749725771ebaef6cbd5661d
Yujin Seol <66419086+rkdfc93@users.noreply.github.com>
2022-05-12 20:20:12 +0900

    url인 경우의 처리 추가

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import io
-from PIL import Image
 
+import requests
+from PIL import Image
+import urllib
 import torch
 from flask import Flask, request
 
@@ -15,7 +17,6 @@ def hello_world():
 def predict():
     if not request.method == "POST":
         return
-
     if request.files.get("image"):
         image_file = request.files["image"]
         image_bytes = image_file.read()
@@ -26,21 +27,17 @@ def predict():
         data = results.pandas().xyxy[0].to_json(orient="records")
         return data
     if request.files.get("url"):
-        image_url = request.files["url"]
-        image_bytes = image_url.read()
-        print(image_url)
-        img = Image.open(io.BytesIO(image_bytes))
+        url_file = request.files["url"]
+        url = url_file.read().decode("UTF-8")
+        res = urllib.request.urlopen(url).read()
 
+        img = Image.open(io.BytesIO(res))
         results = model(img, size=640)
         data = results.pandas().xyxy[0].to_json(orient="records")
         return data
 
-
 if __name__ == "__main__":
-
     model = torch.hub.load(
          "ultralytics/yolov5", "custom", path="last.pt", force_reload=True)
     model.eval()
     app.run(host="0.0.0.0", port=5000)
-    res = request.post("http://127.0.0.1:5000", files = {"url": "http://ec2-3-39-141-76.ap-northeast-2.compute.amazonaws.com/media/admin/%EC%BB%A8%ED%95%981734/22.jpg"}).json()
-    print(res)
